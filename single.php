2b07819c53ab8db2b96115445ba667c25925abe9
David Tahim <davidtahim@gmail.com>
2023-10-16 16:08:22 -0300

    style a página single

---
+++ single.php
@@ -0,0 +1,24 @@
+<?php get_header();?>
+<?php
+ if(have_posts()) {
+  while(have_posts()) {
+    the_post(); ?>
+
+
+  <?php if(the_post_thumbnail()) { ?>
+    <div>
+      <?php the_post_thumbnail() ?>
+    </div>
+  <?php } ?>
+
+  <a href="<?php the_permalink(); ?>"><h1><?php the_title(); ?></h1></a>
+  
+  <!-- Conteudo -->
+  <div>
+    <?php the_content(); ?>
+  </div>
+
+<?php }
+} ?>
+
+<?php get_footer() ?>
